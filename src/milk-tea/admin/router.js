2b951076842d5a7535fc4f91867a456f797105ad
AnhVu-Josep <vulva0201@gmail.com>
2025-10-03 17:38:06 +0700

    Done create Admin revenue (#34)

--- src/milk-tea/admin/router.js
+++ src/milk-tea/admin/router.js
@@ -2,10 +2,12 @@ import Category from './components/AdminCategoryManage.vue'
 import Order from './components/AdminOrderManage.vue'
 import Product from './components/AdminProductManage.vue'
 import Voucher from './components/AdminVoucherManage.vue'
+import Statistics from './components/AdminStatistics.vue'
 
 export default [
   { path: 'category', name: 'AdminCategory', component: Category },
   { path: 'order', name: 'AdminOrder', component: Order },
   { path: 'product', name: 'AdminProduct', component: Product },
-  { path: 'voucher', name: 'AdminVoucher', component: Voucher}
+  { path: 'voucher', name: 'AdminVoucher', component: Voucher},
+  { path: 'statistics', name: 'AdminStatistics', component: Statistics }
 ]
--- src/milk-tea/admin/views/AdminDashboardView.vue
+++ src/milk-tea/admin/views/AdminDashboardView.vue
@@ -5,12 +5,6 @@ const activeTab = ref('product')
 </script>
 <template>
   <div class="container mt-3">
-    <nav class="mb-4">
-      <router-link to="/admin/category" class="btn btn-outline-primary me-2">Categories</router-link>
-      <router-link to="/admin/order" class="btn btn-outline-primary me-2">Order</router-link>
-      <router-link to="/admin/product" class="btn btn-outline-primary me-2">Product</router-link>
-      <router-link to="/admin/voucher" class="btn btn-outline-primary me-2">Voucher</router-link>
-    </nav>
 
     <router-view />
   </div>
